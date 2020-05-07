174736534d6f91ab5c5adb6cd609ef1cc97286c6
gabrielvrl <gabrielvarelaneto11@gmail.com>
2020-05-06 22:05:19 -0300

    backend stuff

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,12 +1,24 @@
 const express = require('express');
+const crypto = require('crypto');
+const connection = require('./database/connection')
 
 const routes = express.Router();
 
-routes.post('/users', (require, response) => {
-    return response.json({ 
-        evento: 'Semana Omnistack 11.0',
-        aluno: 'Gabriel Varela'
-     })
+routes.post('/ongs', async (request, response) => {
+    const { name, email, whatsapp, city, uf } = request.body;
+
+    const id = crypto.randomBytes(4).toString('HEX');
+
+    await connection('ongs').insert({
+        id,
+        name,
+        email,
+        whatsapp,
+        city,
+        uf,
+    })
+
+    return response.json({ id });
 })
 
 module.exports = routes;
